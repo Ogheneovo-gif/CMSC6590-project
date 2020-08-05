34327f591bd1774bc8d13f7a8b47c45647a59ee3
Ogheneovo Mclarry Eduiyovwiri <meduiyovwiri@mun.ca>
2020-08-05 18:59:33 -0230

    Contour plot of the velocity of torsional waves in a full sphere

--- myplot1.py
+++ myplot1.py
@@ -6,4 +6,8 @@ data_file = './cox_etal_2013.txt'  #Data file
 data = np.genfromtxt(data_file,delimiter=',')
 
 f.image_filename = 'myplot1.png'
-f.plot_cylinders(animate=False, save=True, time_idx=0)
+
+plt.plot(u)
+plt.colorbar()
+plt.xlabel('Time(years)')
+plt.ylabel('Radius(km)')
