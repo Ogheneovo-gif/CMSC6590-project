9affc8ec1c909286038b5cfa4694438778e59e94
Ogheneovo Mclarry Eduiyovwiri <meduiyovwiri@mun.ca>
2020-08-05 19:03:20 -0230

    2D plot

--- myplot3.py
+++ myplot3.py
@@ -4,4 +4,4 @@ data_file = './cox_etal_2013.txt'  #Data file
 data = np.genfromtxt(data_file,delimiter=',')
 
 f.image_filename = 'myplot3.png'
-f.plot_contours(animate=False, save=False, time_idx=0)
+f.plot_cylinders(animate=False, save=False, time_idx=0)
